7b351703ce775d91119eb7720aed93948d0db1ea
JOEL B EAPEN <102657079+joel-eapen@users.noreply.github.com>
2026-04-28 19:40:46 +0530

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-// Store your API keys here.
-// IMPORTANT: Do not expose this file in public repositories or production environments.
-
-const CONFIG = {
-    GEMINI_API_KEY: "AIzaSyC4ZfV9ZQvhMBpjsejJcKX_2-4DtiI1eUg"
-}
